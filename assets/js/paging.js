2a9fee584cb7807fdb89fd27780b66b59f8dcf3b
QwertyQwh <qinweihang19988@Outlook.com>
2022-01-09 21:35:40 +0800

    pagination

---
+++ assets/js/paging.js
@@ -0,0 +1,42 @@
+$('#pagination').twbsPagination({
+    totalPages: 2,
+    // the current page that show on start
+    startPage: 1,
+  
+    // maximum visible pages
+    visiblePages: 2,
+  
+    initiateStartPageClick: true,
+  
+    
+  
+    // variable name in href template for page number
+    hrefVariable: '{{number}}',
+  
+    // Text labels
+    first: 'First',
+    prev: 'Previous',
+    next: 'Next',
+    last: 'Last',
+  
+    // carousel-style pagination
+    loop: false,
+  
+    // callback function
+    onPageClick: function (event, page) {
+      $('.page-active').removeClass('page-active');
+      $('#page'+page).addClass('page-active');
+      window.location.href = "#main";
+    },
+  
+    // pagination Classes
+    paginationClass: 'pagination',
+    nextClass: 'actions',
+    prevClass: 'actions',
+    lastClass: 'actions',
+    firstClass: 'actions',
+    pageClass: 'actions',
+    activeClass: 'active',
+    disabledClass: 'disabled',
+  anchorClass:'button large previous'
+  });
