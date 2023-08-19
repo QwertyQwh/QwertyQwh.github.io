ef6c645d302d44c7460136379ebc4f70488aa16c
QwertyQwh <qinweihang19988@Outlook.com>
2023-08-19 17:01:43 -0400

    adjust arg

--- src/Pages/Sections/ColorieShowcase.jsx
+++ src/Pages/Sections/ColorieShowcase.jsx
@@ -1,11 +1,10 @@
 import {  useTexture,OrthographicCamera,PerspectiveCamera,OrbitControls } from '@react-three/drei';
 import { useFBX,useGLTF } from '@react-three/drei';
-import { forwardRef, useImperativeHandle } from 'react';
+import { forwardRef, useImperativeHandle,useEffect } from 'react';
 import keyboard_Roughness from '../../assets/textures/board_Roughness.png'
 import keyboard_Displace from '../../assets/textures/board_Displacement.png'
 import keyboard_Base from '../../assets/textures/board_BaseColor.png'
 import keyboard_Emission from '../../assets/textures/board_Emission.png'
-import { useEffectOnce, useEventListener, useInterval, useWindowSize } from 'usehooks-ts';
 import { useRef } from 'react';
 import { EffectComposer,Bloom } from '@react-three/postprocessing';
 export default forwardRef(function  ColorieShowcase(props,ref){
@@ -18,48 +17,11 @@ export default forwardRef(function  ColorieShowcase(props,ref){
     txtr_keyboard_Displace.flipY = false
     txtr_keyboard_Roughness.flipY = false
     txtr_keyboard_Emission.flipY = false
-    const showcase_ref = useRef()
-    const {width,height} = useWindowSize()
-    const ref_camera = useRef()
-    const handleWindowMouseMove = e=>{
-         ref_camera.current.position.x = -(e.clientX/width-0.5)*3
-         ref_camera.current.position.y = (e.clientY/height-0.5)*3
-    }
-    const targetRotation = useRef(0)
-    const rotateToPi = useRef(false)
-    useEventListener('mousemove',handleWindowMouseMove,[])
-    useImperativeHandle(
-      ref,() => {
-        return{
-            Rotate(){
-                rotateToPi.current = !rotateToPi.current
-                targetRotation.current = rotateToPi.current? Math.PI:0
-                
-            }   
-        }
-      },
-      [],
-    )
-    useInterval(()=>{
 
-        if(Math.abs(showcase_ref.current.rotation.z-targetRotation.current)<0.01){
-            return
-        }
-        showcase_ref.current.rotateZ(0.03*(Math.abs(targetRotation.current-showcase_ref.current.rotation.z)))
 
-    },15)
-    console.log(model)
+
     return  (<>
-        <directionalLight  intensity={1.5}position={[-10, 0, 10]}/>
-        <pointLight intensity={0.8}position={[-5, -5,10]} />
-        <pointLight intensity={0.3}position={[-5, -5,-10]} />
-        <ambientLight intensity={0.5}/>
-        <PerspectiveCamera makeDefault position={[0, 0, 30]} zoom={0.8} ref={ref_camera} />
-        <OrbitControls enableZoom={false} enablePan={false} enableRotate ={false}  enableDamping makeDefault/>
 
-
-        <group rotation={[Math.PI*0.5,Math.PI*0.25,0]} ref = {showcase_ref} >
-            
         {/* <mesh geometry={model.nodes.outline.geometry} position={[0,0,0]} scale={1.6}>
         <meshBasicMaterial color={"#ba957b"} depthWrite={false} />
         </mesh> */}
@@ -72,7 +34,7 @@ export default forwardRef(function  ColorieShowcase(props,ref){
         <mesh geometry={model.nodes.outline.geometry} >
         <meshBasicMaterial  color={'#111111'} />
         </mesh>
-        </group>
+
 
         </>)
 })
--- src/Pages/Sections/Section_Colorie.jsx
+++ src/Pages/Sections/Section_Colorie.jsx
@@ -1,12 +1,13 @@
-import { useRef } from 'react';
+import { useRef,useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { ACESFilmicToneMapping,LinearToneMapping,CineonToneMapping,ReinhardToneMapping,NoToneMapping } from 'three';
 import { Perf } from 'r3f-perf';
 import ColorieShowcase from './ColorieShowcase';
-import { OrbitControls } from '@react-three/drei';
 import anime from 'animejs';
-import { useEffectOnce } from 'usehooks-ts';
 import { useControls,button } from 'leva';
+import { useEffectOnce, useEventListener, useInterval, useWindowSize } from 'usehooks-ts';
+import { PerspectiveCamera,OrbitControls } from '@react-three/drei';
+
 export default function Section_Colorie(props){
     const ref_canvas = useRef()
     const ref_showcase = useRef()
@@ -23,13 +24,48 @@ export default function Section_Colorie(props){
             duration:1500,
             easing:'easeInOutSine'
         })
-
     })
 
+
+    //Canvas Section
+    const ref_threeObj = useRef()
+    const {width,height} = useWindowSize()
+    const ref_camera = useRef()
+
+    const targetRotation = useRef(0)
+    const rotateToPi = useRef(false)
+    const Rotate = ()=>{
+        rotateToPi.current = !rotateToPi.current
+        targetRotation.current = rotateToPi.current? Math.PI:0
+        
+    }   
     const {foo} = useControls('cursor',{
         foo: button(() => {    
-            ref_showcase.current.Rotate()}),
+            Rotate()}),
     })
+    useEffect(() => {
+        if(height>width){
+            ref_camera.current.zoom = 0.8*width/height
+        }
+    }, [width,height]);
+    useInterval(()=>{
+        if(!ref_threeObj.current){
+            return
+        }
+        if(Math.abs(ref_threeObj.current.rotation.z-targetRotation.current)<0.01){
+            return
+        }
+        ref_threeObj.current.rotateZ(0.03*(Math.abs(targetRotation.current-ref_threeObj.current.rotation.z)))
+
+    },15)
+    const handleWindowMouseMove = e=>{
+        if(!ref_camera.current){
+            return
+        }
+        ref_camera.current.position.x = -(e.clientX/width-0.5)*3
+        ref_camera.current.position.y = (e.clientY/height-0.5)*3
+    }
+    useEventListener('mousemove',handleWindowMouseMove,[])
 
     return (<>
         <div className='colorieBg'>
@@ -40,14 +76,16 @@ export default function Section_Colorie(props){
         <div className='heading_title'>
         <h1>中文and English混杂在一起</h1>
         </div>
-        <div className='date'>
-        March 11, 2023
-        </div>
-        </div>
         <div className='heading_date'>
         Aug 25,  2023
         </div>
         </div>
+
+        <div className='about_1'>
+        This is a bunch of description. 这是一段简介
+        </div>
+        </div>
+ 
         </div>
         </div>
         <div>
@@ -57,14 +95,20 @@ export default function Section_Colorie(props){
         </div>
         <div className= 'cornerBubble'>
         </div>
-        <div >
+        <div className='threeCanvas'>
 
         <Canvas ref={ref_canvas} shadows flat dpr = {[1,2]} gl = {{
             toneMapping: NoToneMapping,
             antialias:true, alpha:true}} className ='canvas'  >
-            
-        
-        <ColorieShowcase ref = {ref_showcase}/>
+                    <directionalLight  intensity={1.5}position={[-10, 0, 10]}/>
+        <pointLight intensity={0.8}position={[-5, -5,10]} />
+        <pointLight intensity={0.3}position={[-5, -5,-10]} />
+        <ambientLight intensity={0.5}/>
+        <PerspectiveCamera makeDefault position={[0, 0, 30]} zoom={0.8} ref={ref_camera} />
+        <OrbitControls enableZoom={false} enablePan={false} enableRotate ={false}  enableDamping makeDefault/>
+        <group rotation={[Math.PI*0.5,Math.PI*0.25,0]} ref = {ref_threeObj} >
+        <ColorieShowcase />
+        </group>
         <Perf position = 'bottom-right' />
         </Canvas>
         </div>
