99d2a1027445cd9f1fb011d945699a05ce73e46c
QwertyQwh <qinweihang19988@Outlook.com>
2023-08-20 20:25:06 -0400

    add icon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import App from './App';
 import './css/main.css'
 import './css/blog.css'
 import './css/colorie.css'
+import Img_Avator from './assets/images/avatar.jpg'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = Img_Avator;
 root.render(
     <App />
 );
