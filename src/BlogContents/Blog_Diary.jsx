4ec81694799c725032a0689c0e1af05182ea1968
QwertyQwh <qinweihang19988@Outlook.com>
2023-08-24 15:54:53 -0400

    blogging

---
+++ src/BlogContents/Blog_Diary.jsx
@@ -0,0 +1,8 @@
+import imgBanner from '../assets/images/test/banner.jpg'
+import imgAcadia from '../assets/images/blogs/acadia/acadia.jpg'
+
+
+export default function Blog_Diary(){
+    return <>
+  
+</>}
